d736733c243a2b4d3aa78bd7560a092510b373ea
TAG <charliematina@gmail.com>
2018-06-26 11:00:24 +1200

    Added elements and stylesheet to login page. Also added FontAwesome react component and Google Fonts link

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
 //import Authentication from './components/Authentication.js';
 import AuthService from './components/AuthService';
+import './Login.css';
+var FontAwesome = require('react-fontawesome');
 
 class Login extends Component {
 	constructor(props) {
@@ -52,15 +54,38 @@ class Login extends Component {
 		const errorMessage = this.state.error ? (<div className="alert alert-danger" role="alert">Error: {this.state.error}</div>) : null;
 		return (
 
-				<div>
-				<GoogleLogin
-					clientId="238046755257-ki0kkc04d49j1cr110n54dfakrgmtuh5.apps.googleusercontent.com"
-					buttonText="Login"
-					onSuccess={this.responseGoogle}
-					onFailure={this.responseGoogle}
-				/>
-				{errorMessage}
+			<div className="container-fluid primary login-page-container align-items-center vh-100">
+				<div className="container">
+					<div className="row justify-content-center login-main-content">
+						<div className="col-12 col-md-6 row justify-content-center align-items-center img-container">
+
+								<img id="login-img" src={'/img/hero.png'}/>
+
+						</div>
+						<div className="col-12 col-md-6 row login-details justify-content-center align-items-center">
+							<div className="col-12 row justify-content-center">
+								<div className="col-12">
+									<h1>Portal</h1>
+								</div>
+								<GoogleLogin
+									clientId="238046755257-ki0kkc04d49j1cr110n54dfakrgmtuh5.apps.googleusercontent.com"
+									onSuccess={this.responseGoogle}
+									onFailure={this.responseGoogle}
+									className="login-button row"
+								>
+									<span>Login with Google</span>
+								</GoogleLogin>
+								{errorMessage}
+							</div>
+						</div>
+					</div>
+					<div className="row justify-content-center login-footer">
+						<div className="col-auto">
+							<p>© small copyright text or something</p>
+						</div>
+					</div>
 				</div>
+			</div>
 		);
 	}
 }
